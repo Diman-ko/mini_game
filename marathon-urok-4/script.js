ec03398ca31e06026e615c0356eae3509f252fad
Diman-ko <dr.komarov@mail.ru>
2022-11-11 18:25:06 +0300

    feat: data scrit & functions variable

--- marathon-urok-4/script.js
+++ marathon-urok-4/script.js
@@ -1,4 +1,5 @@
 const board = document.querySelector('#board')
+const colors = ['red', 'blue', 'green', 'yellow', 'pink',  'purple']
 const SQUARES_NUMBER = 600
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
@@ -10,8 +11,8 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
         // console.log('aheajkl'
     )
 
-    square.addEventListener('mouseover', () =>
-        setColor(square)
+    square.addEventListener('mouseleave', () =>
+        removeColor(square)
     )
 
     board.append(square)
@@ -20,6 +21,12 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
 function setColor(element){
     element.style.backgroundColor = 'red'
 }
-
+function removeColor(element){
+    element.style.backgroundColor = '#1d1d1d'
+}
+function getRandomColor() {
+ const index = Math.floor(Math.random()*colors.length)
+     return colors[index]
+}
 
 
