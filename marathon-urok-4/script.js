580b0fe32904406317f3cc141bc11bea6aa9cca4
Diman-ko <dr.komarov@mail.ru>
2022-11-11 18:08:30 +0300

    feat:add color square

--- marathon-urok-4/script.js
+++ marathon-urok-4/script.js
@@ -5,9 +5,16 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
 
+    square.addEventListener('mouseover', () => {
+        setColor(square)
+        // console.log('aheajkl'
+    })
     board.append(square)
 }
 
+function setColor(element){
+    element.style.backgroundColor = 'red'
+}
 
 
 
