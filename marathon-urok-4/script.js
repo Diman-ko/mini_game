eb07b268de4ad1dc0c3db9201b957f7aff5b8a84
Diman-ko <dr.komarov@mail.ru>
2022-11-11 18:14:25 +0300

    fix: script gabage

--- marathon-urok-4/script.js
+++ marathon-urok-4/script.js
@@ -5,10 +5,15 @@ for (let i = 0; i < SQUARES_NUMBER; i++) {
     const square = document.createElement('div')
     square.classList.add('square')
 
-    square.addEventListener('mouseover', () => {
+    square.addEventListener('mouseover', () =>
         setColor(square)
         // console.log('aheajkl'
-    })
+    )
+
+    square.addEventListener('mouseover', () =>
+        setColor(square)
+    )
+
     board.append(square)
 }
 
