89c9d3ea7265b9f3c91d8930fe2fe9f1f9be4e0f
Diman-ko <dr.komarov@mail.ru>
2022-11-11 17:53:29 +0300

    feat:js sript write & flex-wrap, wrap;

--- marathon-urok-4/script.js
+++ marathon-urok-4/script.js
@@ -1,1 +1,13 @@
+const board = document.querySelector('#board')
+const SQUARES_NUMBER = 600
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    board.append(square)
+}
+
+
+
 
